03f6167cdbefbcd7f73348d9176ad3c6476885be
Kot <umaduy@yandex.ru>
2024-03-09 02:10:32 +0300

    Добавление логотипа.

---
+++ Task_01.py
@@ -0,0 +1,39 @@
+import math
+import random
+
+
+a = int(input("Введите колл-во учеников в классе а: "))
+b = int(input("Введите колл-во учеников в классе b: "))
+c = int(input("Введите колл-во учеников в классе c: "))
+
+print(math.ceil((a + b + c)/2))
+# n = int(input('Введите число n: '))
+# m = int(input('Ведите число m: '))
+# c = n/m
+
+# print(c)
+# name = input('Введите имя: ')
+# if name == 'Vyach':
+#     print('Извините, Vyach, Вам не назначено. Досвидание.')
+# else: 
+#    print(f'Привет {name}!')
+   
+# n = int(input('Введите число n: '))
+# m = int(input('Ведите число m: '))
+# print(round(n/m, 2))
+
+
+
+
+
+
+
+# ar = {1, 8, 3, 7, 9, 2, 1, 8, 2, 5}
+# m = 3
+# sum = 0
+# max = 0
+# count = 0
+
+# for (i = 0; i < ar.length(); i++;) {
+    
+# }
